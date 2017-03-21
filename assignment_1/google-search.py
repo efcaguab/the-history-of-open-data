4d29a3fc1a704713209846fe8b3c808bf2942884
Fernando Cagua <efcaguab@gmail.com>
2017-03-21 14:30:08 +1300

    Ask how many results were obtained instead of creating an empty csv file

--- assignment_1/google-search.py
+++ assignment_1/google-search.py
@@ -18,11 +18,12 @@ for country in countries:
     output_file = './data/google_search_results/'+country+'.csv'
     if not(os.path.exists(output_file)):
         search_results = google.search("Open data "+country, num_page)
-        search_results_list = [[str(country),
-          str(search_results[x].name),
-          str(search_results[x].link),
-          str(search_results[x].description)]
-          for x in range(0,len(search_results))]
-        with open(output_file, 'wb') as output_csv:
-            writer = csv.writer(output_csv)
-            writer.writerows(search_results_list)
+        if len(search_results) > 0:
+            search_results_list = [[str(country),
+              str(search_results[x].name),
+              str(search_results[x].link),
+              str(search_results[x].description)]
+              for x in range(0,len(search_results))]
+            with open(output_file, 'wb') as output_csv:
+                writer = csv.writer(output_csv)
+                writer.writerows(search_results_list)
