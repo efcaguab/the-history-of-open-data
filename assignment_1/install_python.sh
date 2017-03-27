e72207300f8d80da7cca95bd9ec32d9997c06080
Fernando Cagua <efcaguab@gmail.com>
2017-03-27 15:26:50 +1300

    install google scrapper library

--- assignment_1/install_python.sh
+++ assignment_1/install_python.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apt-get update && apt-get install -y
-sudo apt-get install build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
 sudo apt-get install python-minimal -y
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
+pip install Google-Search-API
 git clone https://github.com/efcaguab/open_data_history.git
