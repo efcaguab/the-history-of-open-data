f8d3728b306e632b904463d0bd34b35926921517
Fernando Cagua <efcaguab@gmail.com>
2017-03-30 15:47:07 +1300

    scrap all wayback machine

--- scrap-wayback-machine.R
+++ scrap-wayback-machine.R
@@ -1,8 +1,30 @@
 library(rvest)
+library(magrittr)
 
-archive1 <- read_html("https://web.archive.org/web/*/http://www.open.data.al/")
+data_portals <- read.csv("./data/dataportals.csv")
 
-html_node(archive1, "#sparklineImgId") %>%
-	html_attr("src") %>%
-	stringr::str_split("_") %>%
-	unlist()
+data_portals %<>%
+	plyr::ddply("Country", function(x) {
+		country_results <- "./data/google_search_results/" %>%
+			paste0(x$Country[1], ".csv") %>%
+			read.csv(header = F)
+		country_results[x$Position, ]
+	})
+
+for(i in 1:nrow(data_portals)){
+	message("processing ", data_portals$Country[i])
+	portal_file <-"./data/wayback_portal_history/" %>%
+		paste0(data_portals$Country[i], ".txt")
+	if(!file.exists(portal_file)){
+		tryCatch({
+			portal_history <- "https://web.archive.org/web/*/" %>%
+				paste0(as.character(data_portals$V3[i])) %>%
+				read_html() %>%
+				html_node("#sparklineImgId") %>%
+				html_attr("src")
+			writeLines(portal_history, portal_file)
+		}, error = function(e) return(NA))
+		
+		Sys.sleep(1)  # so that we don't get blocked
+	}
+}
